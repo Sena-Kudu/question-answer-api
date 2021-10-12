95e556fd2e191d4d70be4913e573ae6a614eea25
Sena Kudu <kudusena16@gmail.com>
2021-10-12 16:01:29 +0300

    Kullanıcı login işlemi tamamlandı

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../Models/User');
 const CustomError = require('../helpers/error/CustomError');
 const asyncErrorWrapper = require('express-async-handler');
 const {sendJwtToClient} = require('../helpers/authorization/tokenHelpers');
+const {validateUserInput,comparePassword} = require('../helpers/input/inputHelpers');
 
 const register = asyncErrorWrapper(async(req,res,next) => {
 
@@ -14,10 +15,26 @@ const register = asyncErrorWrapper(async(req,res,next) => {
         password,
         role
     });
-
     
     sendJwtToClient(user,res);
 
+});
+
+const login =  asyncErrorWrapper(async(req,res,next) => {
+
+    const{email,password} = req.body;
+    
+    if(!validateUserInput(email,password)) {
+        return next(new CustomError("Please check your inputs", 400));
+    }
+
+    const user = await User.findOne({email}).select("+password");
+
+    if(!comparePassword(password,user.password)) {
+        return next(new CustomError("Please check your credentials",400));
+    }
+ 
+    sendJwtToClient(user,res);
 });
 
 const getUser = (req,res,next) => {
@@ -36,5 +53,6 @@ const getUser = (req,res,next) => {
 
 module.exports = {
     register,
-    getUser
+    getUser,
+    login
 }
--- routers/auth.js
+++ routers/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {register,getUser} = require('../controllers/auth');
+const {register,getUser,login} = require('../controllers/auth');
 const {getAccessToRoute} = require('../middlewares/authorization/auth');
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 //api/auth
 
 router.post("/register", register);
+router.post("/login", login);
 router.get("/profile", getAccessToRoute, getUser);
 
 module.exports = router;
