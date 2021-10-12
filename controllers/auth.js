ff40bfa797a047bce41b6549d6627e6efaf97742
Sena Kudu <kudusena16@gmail.com>
2021-10-12 16:51:55 +0300

    Kullanıcı logout işlemi tamamlandı

--- controllers/auth.js
+++ controllers/auth.js
@@ -37,6 +37,21 @@ const login =  asyncErrorWrapper(async(req,res,next) => {
     sendJwtToClient(user,res);
 });
 
+const logout = asyncErrorWrapper(async(req,res,next) => {
+
+    const {NODE_ENV} = process.env;
+
+    return res.status(200).cookie({
+        httpOnly: true,
+        expires: new Date(Date.now()),
+        secure: NODE_ENV === "development" ? false : true 
+    }).json({
+        success: true,
+        message: "Logout successfull"
+    });
+
+});  
+
 const getUser = (req,res,next) => {
 
      res
@@ -54,5 +69,6 @@ const getUser = (req,res,next) => {
 module.exports = {
     register,
     getUser,
+    logout,
     login
 }
